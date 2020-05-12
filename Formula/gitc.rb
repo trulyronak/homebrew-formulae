75e0469fe29ac1475e888eadcc178d1b45b20f76
Ronak Shah <ronakshahultimate@gmail.com>
2020-05-12 15:07:17 -0700

    Initial Commit

---
+++ Formula/gitc.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Gitc < Formula
+  desc "Streamlined Git. Saving Precious Key Strokes and Enter Keys, One Installation At A Time"
+  homepage "https://github.com/trulyronak/gitc"
+  url "https://registry.npmjs.org/@trulyronak/gitc/-/gitc-0.0.0.tgz"
+  version "0.0.0"
+  sha256 "fd8a9f41e20f4d46d5d88e277394c1d2a48a5829851eaf40d578af4161f7c14a"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    raise "Test not implemented."
+  end
+end
